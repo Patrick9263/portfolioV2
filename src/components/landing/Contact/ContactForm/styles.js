12a055c57a0c1e5ec8c3075232a65d69886a0cd5
patrick9263 <psmith9263@gmail.com>
2021-07-14 23:38:48 -0400

    convert to tabs

--- src/components/landing/Contact/ContactForm/styles.js
+++ src/components/landing/Contact/ContactForm/styles.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
 
 export const Error = styled.span`
-  color: #ff4136;
+	color: #ff4136;
 `
 
 export const Center = styled.div`
-  text-align: left;
-  h4 {
-    font-weight: normal;
-  }
+	text-align: left;
+	h4 {
+		font-weight: normal;
+	}
 `
 
 export const InputField = styled.div`
-  position: relative;
-  margin-bottom: 1rem;
+	position: relative;
+	margin-bottom: 1rem;
 `
